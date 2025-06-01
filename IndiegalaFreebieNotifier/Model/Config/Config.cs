eef3d1c82702e19ae0ee14df60a6f2d3e2cdc360
azhuge233 <17763056+azhuge233@users.noreply.github.com>
2025-06-02 07:37:55 +0800

    Add autoclaim config attributes

--- IndiegalaFreebieNotifier/Model/Config/Config.cs
+++ IndiegalaFreebieNotifier/Model/Config/Config.cs
@@ -2,5 +2,7 @@
 	public class Config : NotifyConfig {
 		public bool EnableHeadless { get; set; }
 		public int TimeOutMilliSecond { get; set; }
+		public bool EnableAutoClaim { get; set; }
+		public string Cookies { get; set; }
 	}
 }
