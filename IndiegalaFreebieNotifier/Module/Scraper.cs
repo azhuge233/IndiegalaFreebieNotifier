745cab029f2dd38df24d8dccec2b47502734213d
azhuge233 <17763056+azhuge233@users.noreply.github.com>
2025-06-02 07:44:29 +0800

    Add get source by url methods with both playwright and HttpClient.

--- IndiegalaFreebieNotifier/Module/Scraper.cs
+++ IndiegalaFreebieNotifier/Module/Scraper.cs
@@ -1,15 +1,17 @@
-﻿using System;
-using System.Threading.Tasks;
-using System.Collections.Generic;
-using HtmlAgilityPack;
-using Microsoft.Playwright;
+﻿using IndiegalaFreebieNotifier.Model;
 using Microsoft.Extensions.Logging;
-using IndiegalaFreebieNotifier.Model;
+using Microsoft.Playwright;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading.Tasks;
 
 namespace IndiegalaFreebieNotifier.Module {
 	class Scraper : IDisposable {
 		private readonly ILogger<Scraper> _logger;
 		private readonly string homeUrl = "https://freebies.indiegala.com/";
+
+		private HttpClient Client { get; set; } = new();
 
 		public Scraper(ILogger<Scraper> logger) {
 			_logger = logger;
@@ -19,25 +21,32 @@ namespace IndiegalaFreebieNotifier.Module {
 
 		public async Task<string> GetHomeSource(Config config) {
 			try {
-				_logger.LogDebug("Getting page source");
-				var webGet = new HtmlDocument();
-
-				var playwright = await Playwright.CreateAsync();
-				await using var browser = await playwright.Firefox.LaunchAsync(new() { Headless = config.EnableHeadless });
-
-				var page = await browser.NewPageAsync();
-				page.SetDefaultTimeout(config.TimeOutMilliSecond);
-				page.SetDefaultNavigationTimeout(config.TimeOutMilliSecond);
-				await page.RouteAsync("**/*", async route => {
-					var blockList = new List<string> { "stylesheet", "image", "font" };
-					if (blockList.Contains(route.Request.ResourceType)) await route.AbortAsync();
-					else await route.ContinueAsync();
-				});
+				_logger.LogDebug("Getting home page source");
+				var page = await GetNewPageInstance(config);
 
 				await page.GotoAsync(homeUrl);
 				await page.WaitForLoadStateAsync();
+
 				var source = await page.InnerHTMLAsync("*");
+
 				await page.CloseAsync();
+
+				_logger.LogDebug("Done");
+				return source;
+			} catch (Exception) {
+				_logger.LogError("Scraping Error");
+				throw;
+			} finally {
+				Dispose();
+			}
+		}
+
+		public async Task<string> GetPageSource(string url) {
+			try {
+				_logger.LogDebug($"Getting page source: {url}");
+				var resp = await Client.GetAsync(url);
+
+				var source = await resp.Content.ReadAsStringAsync();
 
 				_logger.LogDebug("Done");
 				return source;
@@ -52,23 +61,14 @@ namespace IndiegalaFreebieNotifier.Module {
 		public async Task<string> GetPageSource(string url, Config config) {
 			try {
 				_logger.LogDebug($"Getting page source: {url}");
-				var webGet = new HtmlDocument();
 
-				var playwright = await Playwright.CreateAsync();
-				await using var browser = await playwright.Firefox.LaunchAsync(new() { Headless = config.EnableHeadless });
-
-				var page = await browser.NewPageAsync();
-				page.SetDefaultTimeout(config.TimeOutMilliSecond);
-				page.SetDefaultNavigationTimeout(config.TimeOutMilliSecond);
-				await page.RouteAsync("**/*", async route => {
-					var blockList = new List<string> { "stylesheet", "image", "font" };
-					if (blockList.Contains(route.Request.ResourceType)) await route.AbortAsync();
-					else await route.ContinueAsync();
-				});
+				var page = await GetNewPageInstance(config);
 
 				await page.GotoAsync(url);
 				await page.WaitForLoadStateAsync();
+
 				var source = await page.InnerHTMLAsync("*");
+
 				await page.CloseAsync();
 
 				_logger.LogDebug("Done");
@@ -79,6 +79,22 @@ namespace IndiegalaFreebieNotifier.Module {
 			} finally {
 				Dispose();
 			}
+		}
+
+		private async Task<IPage> GetNewPageInstance(Config config) {
+			var playwright = await Playwright.CreateAsync();
+			await using var browser = await playwright.Firefox.LaunchAsync(new() { Headless = config.EnableHeadless });
+
+			var page = await browser.NewPageAsync();
+			page.SetDefaultTimeout(config.TimeOutMilliSecond);
+			page.SetDefaultNavigationTimeout(config.TimeOutMilliSecond);
+			await page.RouteAsync("**/*", async route => {
+				var blockList = new List<string> { "stylesheet", "image", "font" };
+				if (blockList.Contains(route.Request.ResourceType)) await route.AbortAsync();
+				else await route.ContinueAsync();
+			});
+
+			return page;
 		}
 
 		public void Dispose() {
