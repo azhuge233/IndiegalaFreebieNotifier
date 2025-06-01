b9c62a83e9f65834f4bdb39f6630b36962a43bc7
azhuge233 <17763056+azhuge233@users.noreply.github.com>
2025-06-01 22:39:15 +0800

    Add generic page method in scraper.

--- IndiegalaFreebieNotifier/Module/Scraper.cs
+++ IndiegalaFreebieNotifier/Module/Scraper.cs
@@ -9,7 +9,7 @@ using IndiegalaFreebieNotifier.Model;
 namespace IndiegalaFreebieNotifier.Module {
 	class Scraper : IDisposable {
 		private readonly ILogger<Scraper> _logger;
-		private readonly string url = "https://freebies.indiegala.com/";
+		private readonly string homeUrl = "https://freebies.indiegala.com/";
 
 		public Scraper(ILogger<Scraper> logger) {
 			_logger = logger;
@@ -17,7 +17,39 @@ namespace IndiegalaFreebieNotifier.Module {
 			Microsoft.Playwright.Program.Main(["install", "firefox"]);
 		}
 
-		public async Task<string> GetHtmlSource(Config config) {
+		public async Task<string> GetHomeSource(Config config) {
+			try {
+				_logger.LogDebug("Getting page source");
+				var webGet = new HtmlDocument();
+
+				var playwright = await Playwright.CreateAsync();
+				await using var browser = await playwright.Firefox.LaunchAsync(new() { Headless = config.EnableHeadless });
+
+				var page = await browser.NewPageAsync();
+				page.SetDefaultTimeout(config.TimeOutMilliSecond);
+				page.SetDefaultNavigationTimeout(config.TimeOutMilliSecond);
+				await page.RouteAsync("**/*", async route => {
+					var blockList = new List<string> { "stylesheet", "image", "font" };
+					if (blockList.Contains(route.Request.ResourceType)) await route.AbortAsync();
+					else await route.ContinueAsync();
+				});
+
+				await page.GotoAsync(homeUrl);
+				await page.WaitForLoadStateAsync();
+				var source = await page.InnerHTMLAsync("*");
+				await page.CloseAsync();
+
+				_logger.LogDebug("Done");
+				return source;
+			} catch (Exception) {
+				_logger.LogError("Scraping Error");
+				throw;
+			} finally {
+				Dispose();
+			}
+		}
+
+		public async Task<string> GetPageSource(string url, Config config) {
 			try {
 				_logger.LogDebug("Getting page source");
 				var webGet = new HtmlDocument();
