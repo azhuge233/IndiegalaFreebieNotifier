8e4a65a5149d7d5e7e7201eac26357740a8f75f1
azhuge233 <17763056+azhuge233@users.noreply.github.com>
2025-06-02 13:27:30 +0800

    Remove playwrght related configs.

--- IndiegalaFreebieNotifier/Model/Config/Config.cs
+++ IndiegalaFreebieNotifier/Model/Config/Config.cs
@@ -1,7 +1,5 @@
 ﻿namespace IndiegalaFreebieNotifier.Model {
 	public class Config : NotifyConfig {
-		public bool EnableHeadless { get; set; }
-		public int TimeOutMilliSecond { get; set; }
 		public bool EnableAutoClaim { get; set; }
 		public string Cookies { get; set; }
 	}
