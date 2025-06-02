ccd1f6c01bc37d1cf2b834204a7b201a5fd28b8e
azhuge233 <17763056+azhuge233@users.noreply.github.com>
2025-06-02 13:00:32 +0800

    Update XPath

--- IndiegalaFreebieNotifier/Model/String/ParseStrings.cs
+++ IndiegalaFreebieNotifier/Model/String/ParseStrings.cs
@@ -1,8 +1,8 @@
 ﻿namespace IndiegalaFreebieNotifier.Model {
 	public static class ParseStrings {
 		#region XPath strings
-		public static readonly string divXpath = "//div[@class=\"products-col-inner box-shadow-1 relative\"]";
-		public static readonly string titleXpath = ".//div[@class=\"left product-title\"]";
+		public static readonly string divXpath = ".//div[@class=\'products-col-inner box-shadow-1 relative\']";
+		public static readonly string titleXpath = ".//div[@class=\'left product-title\']";
 		public static readonly string linkXpath = ".//a";
 		public static readonly string imgXpath = ".//figure/img";
 		#endregion
