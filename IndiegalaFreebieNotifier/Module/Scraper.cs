0cbc0be474e45ffeea861d5b682421cbe5f47a2f
azhuge233 <17763056+azhuge233@users.noreply.github.com>
2025-06-02 13:01:49 +0800

    Remove Playwright and related functions.

--- IndiegalaFreebieNotifier/Module/Scraper.cs
+++ IndiegalaFreebieNotifier/Module/Scraper.cs
@@ -1,10 +1,8 @@
-﻿using IndiegalaFreebieNotifier.Model;
-using Microsoft.Extensions.Logging;
-using Microsoft.Playwright;
+﻿using Microsoft.Extensions.Logging;
 using System;
-using System.Collections.Generic;
 using System.Net.Http;
 using System.Threading.Tasks;
+// using Microsoft.Playwright;
 
 namespace IndiegalaFreebieNotifier.Module {
 	class Scraper : IDisposable {
@@ -16,7 +14,7 @@ namespace IndiegalaFreebieNotifier.Module {
 		public Scraper(ILogger<Scraper> logger) {
 			_logger = logger;
 			// From https://github.com/microsoft/playwright-dotnet/issues/1545#issuecomment-865199736
-			Microsoft.Playwright.Program.Main(["install", "firefox"]);
+			// Microsoft.Playwright.Program.Main(["install", "firefox"]);
 		}
 
 		public async Task<string> GetHomeSource() {
@@ -36,27 +34,27 @@ namespace IndiegalaFreebieNotifier.Module {
 			}
 		}
 
-		public async Task<string> GetHomeSource(Config config) {
-			try {
-				_logger.LogDebug("Getting home page source");
-				var page = await GetNewPageInstance(config);
+		//public async Task<string> GetHomeSource(Config config) {
+		//	try {
+		//		_logger.LogDebug("Getting home page source");
+		//		var page = await GetNewPageInstance(config);
 
-				await page.GotoAsync(homeUrl);
-				await page.WaitForLoadStateAsync();
+		//		await page.GotoAsync(homeUrl);
+		//		await page.WaitForLoadStateAsync();
 
-				var source = await page.InnerHTMLAsync("*");
+		//		var source = await page.InnerHTMLAsync("*");
 
-				await page.CloseAsync();
+		//		await page.CloseAsync();
 
-				_logger.LogDebug("Done");
-				return source;
-			} catch (Exception) {
-				_logger.LogError("Scraping Error");
-				throw;
-			} finally {
-				Dispose();
-			}
-		}
+		//		_logger.LogDebug("Done");
+		//		return source;
+		//	} catch (Exception) {
+		//		_logger.LogError("Scraping Error");
+		//		throw;
+		//	} finally {
+		//		Dispose();
+		//	}
+		//}
 
 		public async Task<string> GetPageSource(string url) {
 			try {
@@ -75,44 +73,44 @@ namespace IndiegalaFreebieNotifier.Module {
 			}
 		}
 
-		public async Task<string> GetPageSource(string url, Config config) {
-			try {
-				_logger.LogDebug($"Getting page source: {url}");
+		//public async Task<string> GetPageSource(string url, Config config) {
+		//	try {
+		//		_logger.LogDebug($"Getting page source: {url}");
 
-				var page = await GetNewPageInstance(config);
+		//		var page = await GetNewPageInstance(config);
 
-				await page.GotoAsync(url);
-				await page.WaitForLoadStateAsync();
+		//		await page.GotoAsync(url);
+		//		await page.WaitForLoadStateAsync();
 
-				var source = await page.InnerHTMLAsync("*");
+		//		var source = await page.InnerHTMLAsync("*");
 
-				await page.CloseAsync();
+		//		await page.CloseAsync();
 
-				_logger.LogDebug("Done");
-				return source;
-			} catch (Exception) {
-				_logger.LogError("Scraping Error");
-				throw;
-			} finally {
-				Dispose();
-			}
-		}
+		//		_logger.LogDebug("Done");
+		//		return source;
+		//	} catch (Exception) {
+		//		_logger.LogError("Scraping Error");
+		//		throw;
+		//	} finally {
+		//		Dispose();
+		//	}
+		//}
 
-		private async Task<IPage> GetNewPageInstance(Config config) {
-			var playwright = await Playwright.CreateAsync();
-			await using var browser = await playwright.Firefox.LaunchAsync(new() { Headless = config.EnableHeadless });
+		//private async Task<IPage> GetNewPageInstance(Config config) {
+		//	var playwright = await Playwright.CreateAsync();
+		//	var browser = await playwright.Firefox.LaunchAsync(new() { Headless = config.EnableHeadless });
 
-			var page = await browser.NewPageAsync();
-			page.SetDefaultTimeout(config.TimeOutMilliSecond);
-			page.SetDefaultNavigationTimeout(config.TimeOutMilliSecond);
-			await page.RouteAsync("**/*", async route => {
-				var blockList = new List<string> { "stylesheet", "image", "font" };
-				if (blockList.Contains(route.Request.ResourceType)) await route.AbortAsync();
-				else await route.ContinueAsync();
-			});
+		//	var page = await browser.NewPageAsync();
+		//	page.SetDefaultTimeout(config.TimeOutMilliSecond);
+		//	page.SetDefaultNavigationTimeout(config.TimeOutMilliSecond);
+		//	await page.RouteAsync("**/*", async route => {
+		//		var blockList = new List<string> { "stylesheet", "image", "font" };
+		//		if (blockList.Contains(route.Request.ResourceType)) await route.AbortAsync();
+		//		else await route.ContinueAsync();
+		//	});
 
-			return page;
-		}
+		//	return page;
+		//}
 
 		public void Dispose() {
 			GC.SuppressFinalize(this);
