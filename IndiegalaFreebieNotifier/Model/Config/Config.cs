7faec739ea6664ab95f6110ef77f0ce8a8bd09f3
azhuge233 <17763056+azhuge233@users.noreply.github.com>
2025-06-25 21:41:14 +0800

    Add cookie auto refresh related config options.

--- IndiegalaFreebieNotifier/Model/Config/Config.cs
+++ IndiegalaFreebieNotifier/Model/Config/Config.cs
@@ -2,5 +2,9 @@
 	public class Config : NotifyConfig {
 		public bool EnableAutoClaim { get; set; }
 		public string Cookies { get; set; }
+		public bool EnableCookieAutoRefresh { get; set; }
+		public string TwoCaptchaApiKey { get; set; }
+		public string Username { get; set; }
+		public string Password { get; set; }
 	}
 }
